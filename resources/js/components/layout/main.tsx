d3cc6031d4cc1e2722f1f2969e9940cbdf4a7306
abdullahnavico <m.abdullah@navicosoft.com>
2026-06-03 03:43:15 +0500

    fix: correct overflow property in Main component for fixed layout

--- resources/js/components/layout/main.tsx
+++ resources/js/components/layout/main.tsx
@@ -14,7 +14,7 @@ export function Main({ fixed, className, fluid, ...props }: MainProps) {
         'px-4 py-6',
 
         // If layout is fixed, make the main container flex and grow
-        fixed && 'flex grow flex-col overflow-clip',
+        fixed && 'flex grow flex-col overflow-hidden',
 
         // If layout is not fluid, set the max-width
         !fluid &&
