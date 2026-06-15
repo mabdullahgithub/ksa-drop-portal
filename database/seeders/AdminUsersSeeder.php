d995039a5f4eb68b457344604258d37781c0109c
abdullahnavico <m.abdullah@navicosoft.com>
2026-06-15 23:36:14 +0500

    feat: add AdminUsersSeeder to seed initial admin users and roles

---
+++ database/seeders/AdminUsersSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUsersSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Support',
+                'email' => 'support@ksadrop.com',
+                'password' => Hash::make('KsaDrop$123'),
+                'role' => 'superadmin',
+            ],
+            [
+                'name' => 'Developer',
+                'email' => 'developer@ksadrop.com',
+                'password' => Hash::make('password'),
+                'role' => 'superadmin',
+            ],
+        ];
+
+        foreach ($users as $data) {
+            $user = User::firstOrCreate(
+                ['email' => $data['email']],
+                [
+                    'name' => $data['name'],
+                    'password' => $data['password'],
+                    'email_verified_at' => now(),
+                ]
+            );
+
+            if (! $user->hasRole($data['role'])) {
+                $user->assignRole($data['role']);
+            }
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(AdminUsersSeeder::class);
         $this->call(PermissionCategoriesSeeder::class);
         $this->call(OrdersSeeder::class);
     }
