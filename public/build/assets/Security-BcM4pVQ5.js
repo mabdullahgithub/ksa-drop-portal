514daf0bd628a94349cc508bf85ab7be13eb495a
muhammad abdullah <142112488+mabdullahgithub@users.noreply.github.com>
2026-09-22 16:35:38 +0500

    feat: add new components for UI enhancements including sidebar navigation, tabs, and theme switcher
    - Implemented `show-submitted-data` for displaying submitted values.
    - Created `sidebar-nav` for improved navigation experience.
    - Added `switch` component for toggle functionality.
    - Developed `tabs` component for better content organization.
    - Introduced `theme-switch` for dynamic theme changes.
    - Added `use-permissions` hook for managing user permissions and roles.

--- public/build/assets/Security-BcM4pVQ5.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-C7oxC63R.js";import{t}from"./AuthenticatedLayout-oKJjSbha.js";import{t as n}from"./security-form-Bbeke9La.js";import{y as r}from"./app-DVY8zmRJ.js";import{t as i}from"./settings-ke9B6w6q.js";var a=e();function o(){return(0,a.jsxs)(t,{children:[(0,a.jsx)(r,{title:`Security`}),(0,a.jsx)(i,{children:(0,a.jsx)(n,{})})]})}export{o as default};
